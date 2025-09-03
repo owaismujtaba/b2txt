4f97db8bb89f20de9d721f922b423e9b8ee10745
Owais Mujtaba Khanday <45037843+owaismujtaba@users.noreply.github.com>
2025-09-03 16:32:57 +0200

    Update rnn_trainer.py

--- model_training/rnn_trainer.py
+++ model_training/rnn_trainer.py
@@ -84,17 +84,17 @@ class BrainToTextDecoder_Trainer:
 
         # Configure device pytorch will use 
         if torch.cuda.is_available():
+            self.device = torch.device("cuda:0")
             print('GPU Available')
             num_gpus = torch.cuda.device_count()
             if num_gpus > 1:
                 print('Good Multiple GPU')
-                device = torch.device("cuda:0")  # main device
                 self.multi_gpu = True
             else:
-                device = torch.device("cuda:0")
+                
                 self.multi_gpu = False
         else:
-            device = torch.device("cpu")
+            self.device = torch.device("cpu")
             self.multi_gpu = False
         
         self.logger.info(f'Using device: {self.device}')
