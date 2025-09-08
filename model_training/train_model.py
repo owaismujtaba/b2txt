88dc3b143fa69c903c17d5526befdd48a0756424
Owais Mujtaba Khanday <45037843+owaismujtaba@users.noreply.github.com>
2025-09-08 14:42:26 +0200

    Update train_model.py

--- model_training/train_model.py
+++ model_training/train_model.py
@@ -1,6 +1,6 @@
 from omegaconf import OmegaConf
 from rnn_trainer import BrainToTextDecoder_Trainer
 
-args = OmegaConf.load('/kaggle/working/b2txt/model_training/rnn_args.yaml')
+args = OmegaConf.load('model_training/rnn_args.yaml')
 trainer = BrainToTextDecoder_Trainer(args)
 metrics = trainer.train()
